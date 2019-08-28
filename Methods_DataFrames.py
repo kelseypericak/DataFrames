eba3bdadd649e45d58a9d1b228270979535860ae
kevic <kelspericak@gmail.com>
2019-08-27 23:58:20 -0400

    Update Methods_DataFrames.py

--- Methods_DataFrames.py
+++ Methods_DataFrames.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 """
 Created on Tue Aug 27 10:16:28 2019
 
 @author: kpericak
 
-Cosmetics example: Marc Jacobs lip makeup on Sephora, extracted from site Aug '19
+Cosmetics example: Marc Jacobs lip makeup on Sephora, mined from website Aug 2019
 
 """
 
@@ -16,37 +18,35 @@ from tabulate import tabulate
 ##Method 1: List
 print ('Method 1: List input')
 
-listInput = [['Marc Jacobs Beauty', 'Le Marc Lip Crème Lipstick', 42, 'stick', 'single', 34],  
-         ['Marc Jacobs Beauty', 'New Nudes Sheer Gel Lipstick', 42, 'stick', 'single', 9],  
-         ['Marc Jacobs Beauty', 'Le Marc Lip Frost', 42, 'stick', 'single', 9], 
-         ['Marc Jacobs Beauty', 'Le marc Liquid Lip Crayon', 34, 'crayon', 'single', 9],                 
-         ['Marc Jacobs Beauty', 'Make Your Le Marc 3 Piece Mini Liquid Lip Crayon Set', 33,
+listInput = [['Le Marc Lip Crème Lipstick', 42, 'stick', 'single', 34],  
+         ['New Nudes Sheer Gel Lipstick', 42, 'stick', 'single', 9],  
+         ['Le Marc Lip Frost', 42, 'stick', 'single', 9], 
+         ['Le marc Liquid Lip Crayon', 34, 'crayon', 'single', 9],                 
+         ['Make Your Le Marc 3 Piece Mini Liquid Lip Crayon Set', 33,
           'crayon', 'set', 1],                 
-         ['Marc Jacobs Beauty', 'Kiss Pop Lipstick', 35, 'stick', 'single', 8],                 
-         ['Marc Jacobs Beauty', 'Somewhere, Anywhere - Le Marc Liquid Lip Crayon Collection',
+         ['Kiss Pop Lipstick', 35, 'stick', 'single', 8],                 
+         ['Somewhere, Anywhere - Le Marc Liquid Lip Crayon Collection',
           78, 'crayon', 'set', 1],                 
-         ['Marc Jacobs Beauty', 'Le Marc Liquid Lip Crème', 38, 'gloss', 'single', 5],                 
-         ['Marc Jacobs Beauty', 'The Sex Kitten Limited-Edition Set', 26, 'gloss','set', 1],
-         ['Marc Jacobs Beauty', 'Le Marc Jajobs Lip Crème Lipstick in True', 42, 'stick', 
-          'single', 1],
-         ['Marc Jacobs Beauty', 'Fashion Collection Le Marc Lip Crème', 38, 'stick', 'single', 1],
-         ['Marc Jacobs Beauty', 'Collector\'s Edition Le Marc Lip Créme Lipstick - Blacquer', 40, 
-          'stick', 'single', 1],
-         ]    
+         ['Le Marc Liquid Lip Crème', 38, 'gloss', 'single', 5],                 
+         ['The Sex Kitten Limited-Edition Set', 26, 'gloss','set', 1],
+         ['Le Marc Jajobs Lip Crème Lipstick in True', 42, 'stick', 'single', 1],
+         ['Fashion Collection Le Marc Lip Crème', 38, 'stick', 'single', 1],
+         ['Collector\'s Edition Le Marc Lip Crème Lipstick - Blacquer', 40, 'stick', 'single', 1]]    
 
-headers = ['Brand', 'Name', 'Price', 'Type', 'Item', 'ColourOptions']
+headers = ['Name', 'Price', 'Type', 'Item', 'ColourOptions']
 
 print('\nThe list we are inputting looks like this, with the followings headers: ' + str(headers) + '.\n')
 print(listInput)
 
 df_fromList = pd.DataFrame(list(listInput))     #create dataframe
 df_fromList.columns = headers
-pd.set_option('max_colwidth', 800)              #set a max width if the Name is not showing fully
+pd.set_option('max_colwidth', 800)              #set a max width if the Name column is not showing fully
 
 print('\nThe dataframe will look like this using pd.DataFrame(list(listInput)): \n')
 print(tabulate(df_fromList, df_fromList.head(), showindex = False)) #print the table using tabulate
+                                                                    #printing tabulate with python version 2.7 will result in error
 
-df = df_fromList #main dataframe to use to create different data structures in future exmpales
+df = df_fromList #main dataframe to use to create different data structures in future examples
 
 
 
@@ -75,12 +75,14 @@ for column in columns:
     union_list.append(df[column].tolist())  #assume this was your initial input
 
 print('\nThe union of lists looks like this: \n')
-print(union_list)                           #assume this is initial input
+print(union_list)                           
 
 zipped_list = list(zip(*union_list))
 
 df_fromUnionZip = pd.DataFrame(list(zipped_list)) 
-headers = ['Brand', 'Name', 'Price', 'Type', 'Item', 'ColourOptions']
+
+headers = ['Name', 'Price', 'Type', 'Item', 'ColourOptions']
+
 print('\nThe dataframe will look like this using pd.DataFrame(list(zipped_list)) after\
  manipulating the data structure: \n')
 print(tabulate(df_fromUnionZip, headers, showindex = False)) #Print the table using tabulate
